818d181da1a80fbca2b3da77f1ae986916a1cc0d
William Chang <>
2022-01-15 13:43:15 -0800

    Get importing of prost working

---
+++ rust/draw/src/lib.rs
@@ -0,0 +1,9 @@
+// Include the `draw` module, which is generated from image.proto.
+pub mod draw {
+    include!(concat!(env!("OUT_DIR"), "/draw.image.rs"));
+}
+
+pub fn create_image() -> draw::Image {
+    let img = draw::Image::default();
+    img
+}
