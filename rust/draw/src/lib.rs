1696c86aee2222aae3ed0e2be8499563d9b70433
William Chang <>
2022-01-16 12:40:42 -0800

    Get proto module nesting to match the crate module

--- rust/draw/src/lib.rs
+++ rust/draw/src/lib.rs
@@ -1,9 +1,10 @@
 // Include the `draw` module, which is generated from image.proto.
-pub mod draw {
-    include!(concat!(env!("OUT_DIR"), "/draw.image.rs"));
-}
+// pub mod draw {
+//     include!(concat!(env!("OUT_DIR"), "/draw.image.rs"));
+// }
+include!(concat!(env!("OUT_DIR"), "/draw.image.rs"));
 
-pub fn create_image() -> draw::Image {
-    let img = draw::Image::default();
-    img
-}
+// pub fn create_image() -> draw::Image {
+//     let img = draw::Image::default();
+//     img
+// }
