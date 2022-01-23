e2d11ba019676302de0f5cfe9a0adee0d4e04990
William Chang <>
2022-01-23 09:55:21 -0800

    Use image module to draw a gradient png

--- rust/draw/src/main.rs
+++ rust/draw/src/main.rs
@@ -1,6 +1,20 @@
+extern crate image;
+
 fn main() {
-    println!("{:#?}", draw::Rgb::default().red());
-    println!("{:#?}", draw::Rgb::default().green());
-    println!("{:#?}", draw::Rgb::default().blue());
-    println!("{:#?}", draw::Image::default());
+    let imgx = 800;
+    let imgy = 800;
+
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+
+    let err = imgbuf.save("gradient.png");
+    match err {
+        Ok(_) => println!("Saved image to gradient.png"),
+        Err(error) => println!("Error saving to gradient.png: {:?}", error),
+    };
 }
